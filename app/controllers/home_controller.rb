30d5a11254b988c02a4efc7e2fee97cdeb423697
Arfon Smith <arfon.smith@gmail.com>
2013-08-21 13:09:38 -0500

    View changes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @header_title = "Girls Do Hack"
-    @header_text = " is designed to emphasize to young women that STEM careers are an option for their futures. On November 9th 2013, teams of girls will work with women in science, technology, engineering and math careers at the Adler Planetarium."
+    @pagetitle = "Home"
     @youth = User.find(:all, :order => "id desc", :limit => 5).reverse
     @mentors = User.find(:all, :order => "id desc", :limit => 5).reverse
     #@sponsors = Organization.find_all_by_org_type("sponsor")
