e31ba9070c8107d6513bc5b21383bdb4bd92ba73
Laura Whyte <laura.whyte@gmail.com>
2013-08-22 11:15:44 -0500

    randomising the mentor-youth part

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,8 @@
 class HomeController < ApplicationController
   def index
     @pagetitle = "Home"
-    @youth = User.find(:all, :order => "id desc", :limit => 5).reverse
-    @mentors = User.find(:all, :order => "id desc", :limit => 5).reverse
-    #@sponsors = Organization.find_all_by_org_type("sponsor")
+    @youth = User.where(part_type: 'youth').all.shuffle[0..4]
+    @mentors = User.where(part_type: 'mentor').all.shuffle[0..4]
     #@host = Organization.find_by_org_type("host")
     #@partners = Organization.find_by_org_type("partner")
   end
