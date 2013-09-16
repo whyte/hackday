8c65cfb812938295a39fee19be51f622aa9399f8
Laura Whyte <laura.whyte@gmail.com>
2013-09-16 16:12:13 -0500

    adding summaries

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -6,8 +6,8 @@ class AdminController < ApplicationController
       @youth = User.find_all_by_part_type("youth")
       @total_committed_mentors = User.where(:part_type => "mentor", :published => true).count
       @total_committed_youth = User.where(:part_type => "youth", :published => true).count
-      @total_potential_mentors = User.where(:part_type => "mentor", :published => false).count
-      @total_potential_youth = User.where(:part_type => "youth", :published => false).count
+      @total_potential_mentors = User.where(:part_type => "mentor", :published => nil).count
+      @total_potential_youth = User.where(:part_type => "youth", :published => nil).count
       @total_mentors = User.where(:part_type => "mentor").count
       @total_youth = User.where(:part_type => "youth").count
     else
