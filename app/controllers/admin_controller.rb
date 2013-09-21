13b93a918e83138a64f1fb954aced56b383a8b6d
Laura Whyte <laura.whyte@gmail.com>
2013-09-21 16:29:40 -0500

    text change

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,15 +1,18 @@
 class AdminController < ApplicationController
   
   def index
+    expiry_time = Time.parse('21 Sept 2013 15:30:00')
     if current_user.admin?
-      @mentors = User.find_all_by_part_type("mentor")
-      @youth = User.find_all_by_part_type("youth")
-      @total_committed_mentors = User.where(:part_type => "mentor", :published => true).count
+      @mentors = User.where(:part_type => "mentor").where('created_at < ?', expiry_time)
+      @youth = User.where(:part_type => "youth")
+      @total_committed_mentors = User.where(:part_type => "mentor", :published => true).where('created_at < ?', expiry_time).count
       @total_committed_youth = User.where(:part_type => "youth", :published => true).count
-      @total_potential_mentors = User.where(:part_type => "mentor", :published => nil).count
+      @total_potential_mentors = User.where(:part_type => "mentor", :published => nil).where('created_at < ?', expiry_time).count
       @total_potential_youth = User.where(:part_type => "youth", :published => nil).count
-      @total_mentors = User.where(:part_type => "mentor").count
+      @total_mentors = User.where(:part_type => "mentor").where('created_at < ?', expiry_time).count
       @total_youth = User.where(:part_type => "youth").count
+      @mentors_post_close = User.where(:part_type => "mentor").where('created_at > ?', expiry_time)
+      @mentors_post_close_count = User.where(:part_type => "mentor").where('created_at > ?', expiry_time).count
     else
       redirect_to :controller => "home", :action => "index"
     end
